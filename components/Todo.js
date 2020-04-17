5242c7fc0cf0cc8be6dae88807b479d5bbdac4ac
Hanna Sezen <Hanna_Sezen@epam.com>
2020-04-17 22:37:35 +0300

    changed todo`s background

--- components/Todo.js
+++ components/Todo.js
@@ -5,7 +5,6 @@ export const Todo = ({ todo }) => {
     return (
         <View style={styles.todo}>
             <View style={styles.todoDescription}>
-                <Text style={styles.categoryIcon}>*</Text>
                 <Text style={styles.todoText}>{todo.title}</Text>
             </View>
             <TouchableOpacity style={styles.deleteButton}>
@@ -19,32 +18,30 @@ const styles = StyleSheet.create({
     todo: {
         flexDirection: 'row',
         height: 40,
-        backgroundColor: '#999',
         marginBottom: 5,
         borderRadius: 2,
+        backgroundColor: '#E6E7E8',
     },
     todoDescription: {
         flex: 1,
         flexDirection: 'row',
-        alignItems: 'stretch',
-    },
-    categoryIcon: {
-        width: 40,
-        marginRight: 10,
-        textAlign: 'center',
-        textAlignVertical: 'center',
-        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        padding: 10,
     },
     todoText: {
         textAlignVertical: 'center'
     },
     deleteButton: {
-        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
         width: 40,
         marginLeft: 10,
+        borderTopRightRadius: 2,
+        borderBottomRightRadius: 2,
+        backgroundColor: '#D1D3D4',
     },
     deleteText: {
-        textAlign: 'center',
-        textAlignVertical: 'center',
+        fontSize: 20,
     },
 })
